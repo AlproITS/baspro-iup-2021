59c1b2758e5da0924404514880319621094f837f
seraaaz <87944227+seraaaz@users.noreply.github.com>
2021-08-29 23:15:59 +0700

    Add files via upload

---
+++ Problems (Week 1)/DedibayandHisImmortalLife.c
@@ -0,0 +1,23 @@
+//
+//  main.c
+//  Dedibay_S1
+//
+//  Created by Selomita Zhafiirah on 29/08/21.
+//
+
+#include <stdio.h>
+
+int main() {
+    int f1, f2;
+    
+    scanf("%d", &f1);
+    scanf("%d", &f2);
+    
+    if(f1 == f2){
+        printf("YES! DEDIBAY CAN LIVE IMMORTAL\n");
+    }else{
+        printf("NO! GOODBYE DEDIBAY, RIP\n");
+    }
+    
+    return 0;
+}
